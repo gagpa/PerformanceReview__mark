8c7eee289a8cbffc7d1bf2a1e5e95856b6143196
ofry <speitshot@gmail.com>
2021-03-23 14:10:35 +0300

    Сервисы. Оформление

--- app/services/status.py
+++ app/services/status.py
@@ -3,59 +3,46 @@ from app.models import Status, Form
 
 
 def get_new_form():
-    """
-    Вернуть статус для новой формы.
-    :return:
-    """
+    """ Вернуть статус для новой формы """
     status = db_session.query(Status).filter_by(name='Заполнение анкеты').first()
     return status
 
 
 def get_boss_review():
-    """
-
-    :return:
-    """
+    """ Получить статус на оценке босса """
     status = db_session.query(Status).filter_by(name='У руководителя').first()
     return status
 
 
-def get_emp_review():
-    """
-
-    :return:
-    """
+def get_coworker_review():
+    """ Получить статус на оценке коллег """
     status = db_session.query(Status).filter_by(name='На оценке коллег').first()
     return status
 
 
 def change_boss_review(form: Form):
-    """
-    Сменить статус на проверке у руководителя.
-    :return:
-    """
+    """ Сменить статус на проверке у руководителя """
     form.status = get_boss_review()
     db_session.add(form)
     db_session.commit()
 
 
 def change_emp_review(form: Form):
-    """
-    Сменить статус на оценка коллег.
-    :param form:
-    :return:
-    """
-    form.status = get_emp_review()
+    """ Сменить статус на оценка коллег """
+    form.status = get_coworker_review()
     db_session.add(form)
     db_session.commit()
 
 
 def change_fill_form(form: Form):
-    """
-    Сменить статус на заполнение формы.
-    :param form:
-    :return:
-    """
+    """ Сменить статус на заполнение формы """
     form.status = get_new_form()
     db_session.add(form)
     db_session.commit()
+
+
+STATUS_BOSS_REVIEW = get_boss_review()
+
+STATUS_COWORKER_REVIEW = get_coworker_review()
+
+STATUS_WRITE_FORM = get_new_form()
