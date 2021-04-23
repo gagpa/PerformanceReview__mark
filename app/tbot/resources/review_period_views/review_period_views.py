ca36a9228081b0aa0312683b32ad3067d29e2693
alexzhuravlev <aleexzhuravlev@gmail.com>
2021-04-23 16:48:50 +0300

    Рефакторинг

--- app/tbot/resources/review_period_views/review_period_views.py
+++ app/tbot/resources/review_period_views/review_period_views.py
@@ -1,12 +1,9 @@
 import datetime
 
-from loguru import logger
-from telebot.apihelper import ApiTelegramException
-
 from app.db import Session
 from app.models import ReviewPeriod, User, Role
 from app.services.review import ReviewPeriodService
-from app.tbot import bot, notificator
+from app.tbot import notificator
 from app.tbot.services.forms import Notification
 from app.tbot.services.forms.review_period_form import ReviewPeriodForm
 
@@ -36,7 +33,7 @@ def second_date_period(request, date):
     service = ReviewPeriodService()
     service.create(is_active=True, start_date=first_date, end_date=second_date)
     Session.commit()
-    users = Session().query(User).join(Role, User.role)\
+    users = Session().query(User).join(Role, User.role) \
         .filter(Role.name != 'Undefined').all()
     notificator.notificate(Notification(view='start_review'), *[user.chat_id for user in users])
     return ReviewPeriodForm(started=True)
