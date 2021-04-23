569bb12da2b2cdefbf4eb46b29420ac9a1501e6d
alexzhuravlev <aleexzhuravlev@gmail.com>
2021-04-23 16:05:36 +0300

    Добавлена сортировка архива

--- app/tbot/resources/review_period_views/archive_views.py
+++ app/tbot/resources/review_period_views/archive_views.py
@@ -6,5 +6,6 @@ from app.tbot.services.forms.archive_form import ArchiveForm
 def old_forms_list(request):
     old_reviews = Session().query(Form).join(ReviewPeriod, Form.review_period) \
         .join(User, Form.user).join(Status, Form.status) \
-        .filter(ReviewPeriod.is_active == False).all()
+        .filter(ReviewPeriod.is_active == False)\
+        .order_by(ReviewPeriod.end_date.desc()).all()
     return ArchiveForm(models=old_reviews, archive_list=True)
