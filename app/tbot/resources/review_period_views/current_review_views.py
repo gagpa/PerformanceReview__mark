a4fae2ca202e7782afd6c0d68ec1ece771936221
alexzhuravlev <aleexzhuravlev@gmail.com>
2021-04-26 12:16:12 +0300

    Рефакторинг

--- app/tbot/resources/review_period_views/current_review_views.py
+++ app/tbot/resources/review_period_views/current_review_views.py
@@ -1,11 +1,9 @@
 from app.db import Session
-from app.models import Form, ReviewPeriod, User, Status, CoworkerAdvice, Project, \
-    CoworkerProjectRating, CoworkerReview
-from app.services.dictinary import StatusService, HrReviewStatusService
+from app.models import Form, ReviewPeriod, User, Status, CoworkerAdvice
+from app.services.dictinary import StatusService
 from app.services.dictinary.summary import SummaryService
 from app.services.form_review import FormService
 from app.services.form_review.project_comments import ProjectCommentService
-from app.services.review import CoworkerReviewService
 from app.tbot.services.forms.current_review_form import CurrentReviewForm
 
 
@@ -33,7 +31,8 @@ def employee_review(request):
 def input_summary(request):
     """Предлагаем HR ввести summary"""
     form_id = request.args['form_id'][0]
-    return CurrentReviewForm(change_summary=True), request.send_args(change_summary, form_id=form_id)
+    return CurrentReviewForm(change_summary=True), request.send_args(change_summary,
+                                                                     form_id=form_id)
 
 
 def change_summary(request):
@@ -41,23 +40,10 @@ def change_summary(request):
     form_id = request.args['form_id']
     summary = SummaryService().by_form_id(form_id)
     if not summary:
-        summary = SummaryService().create(form_id=form_id, from_hr_id=request.user.id,
+        SummaryService().create(form_id=form_id, from_hr_id=request.user.id,
                                           text=request.text)
         form = FormService().by_pk(form_id)
         StatusService().change_to_done(form)
-        # TODO: изменять статус у оценок
-        # advices = CoworkerReviewService().all_by(form_id=pk)
-        # ratings = Session.query(CoworkerProjectRating). \
-        #     join(Project, CoworkerProjectRating.project).\
-        #     join(CoworkerReview)\
-        #     .filter(Project.form_id == pk).all()
-        # print(ratings)
-        # for rating in ratings:
-        #     rating.hr_review_status = HrReviewStatusService().accept
-        #     print(rating.hr_review_status)
-        #     Session.add(rating)
-        #     Session.commit()
-
     else:
         summary.text = request.text
         Session.add(summary)
