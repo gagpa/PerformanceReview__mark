17c8f69b9cffcd4ce65bc6d60c2ea7f5fd608182
makhonin-sk <makhonin-sk@metro.ru>
2022-01-28 15:05:51 +0300

    Добавлены настройки для удобного деплоя

--- app/db.py
+++ app/db.py
@@ -8,9 +8,6 @@ Base = declarative_base()
 
 engine = create_engine(DATABASE_URI)
 
-# Импорт необходим для создания новых миграций.
-from app.models import *
-
 SessionFactory = sessionmaker(bind=engine, autoflush=False)
 Session = scoped_session(SessionFactory)
 
